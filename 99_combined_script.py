eb83f045a03403c3faf0e861fc0ec5e5d57c3d51
corvuslee <corvus.lee@gmail.com>
2023-07-27 13:08:43 +0100

    try opensearchserverless #3

--- 99_combined_script.py
+++ 99_combined_script.py
@@ -10,24 +10,33 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 
 
-# Parameters - AOSS
-host = os.environ['AOSS_ENDPOINT']
-input_file = 'data/state_of_the_union.txt'
-# input_file = 'data/Donald J. Trump [February 05, 2019].txt'
-region = 'eu-west-1'
-service = 'es'
+# ES/AOSS connection
+es_host = os.environ['ES_ENDPOINT']
+aoss_host = os.environ['AOSS_ENDPOINT']
 credentials = boto3.Session().get_credentials()
-auth = AWSV4SignerAuth(credentials, region, service)
-aoss_client_conf = {
-    "http_auth": auth,
+es_auth = AWSV4SignerAuth(credentials, 'eu-west-1', 'es')
+aoss_auth = AWSV4SignerAuth(credentials, 'eu-west-1', 'aoss')
+es_client_conf = {
+    "http_auth": es_auth,
     "use_ssl": True,
     "verify_certs": True,
     "connection_class": RequestsHttpConnection,
     "pool_maxsize": 20
 }
-index = 'aoss_qa'
+aoss_client_conf = {
+    "http_auth": aoss_auth,
+    "use_ssl": True,
+    "verify_certs": True,
+    "connection_class": RequestsHttpConnection,
+    "pool_maxsize": 20
+}
 
-# Parameters - LangChain
+# Documents, index
+index = 'aoss_qa'
+input_file_1 = 'data/state_of_the_union.txt'
+input_file_2 = 'data/Donald J. Trump [February 05, 2019].txt'
+
+# LangChain
 text_splitter_conf = {
     "chunk_size": 1000,
     "chunk_overlap": 20,
@@ -35,28 +44,30 @@ text_splitter_conf = {
     "add_start_index": True,
 }
 
+# Switches
+host = aoss_host  # or es_host
+input_file = input_file_1
+
 # Load document and split to texts
 loader = UnstructuredFileLoader(input_file)
 text_splitter = RecursiveCharacterTextSplitter(**text_splitter_conf)
 texts = loader.load_and_split(text_splitter)
 
-# Connect to AOSS
+# Connect to OpenSearch
+is_aoss = True if host == aoss_host else False
+client_conf = aoss_client_conf if is_aoss else es_client_conf
+
 embeddings_model = OpenAIEmbeddings()
-aoss = OpenSearchVectorSearch(
+vectorstore = OpenSearchVectorSearch(
     opensearch_url=host,
     index_name=index,
     embedding_function=embeddings_model,
-    **aoss_client_conf
+    is_aoss=is_aoss,
+    **client_conf
 )
-assert aoss.client.ping()
 
-# Load text & embeddings to AOSS
-# Thoughts:
-# - Add documents to an existing index without mapping will result in wrong type
-# - Add documents and create new index will use default no. of shards & replicas
-# - New IDs are generated for each run, introducing duplicated docs in AOSS
-#   Try https://github.com/langchain-ai/langchain/blob/4da43f77e5bf3d25f5b7ece8bcba1ab7c6a9abb2/libs/langchain/langchain/vectorstores/opensearch_vector_search.py#L402C13-L402C16
-text_id_list = aoss.add_documents(
+# Load text & embeddings to OpenSearch
+text_id_list = vectorstore.add_documents(
     texts,
     engine="faiss",
     space_type="l2",
@@ -65,24 +76,15 @@ text_id_list = aoss.add_documents(
 )
 assert len(text_id_list) == len(texts)
 
-# # Delete index
-# aoss.client.indices.delete(index)
-
-# # Check index status
-# print(aoss.client.indices.stats(index))
-
-# # Check index mapping
-# print(aoss.client.indices.get_mapping(index))
-
 # # Check first item
-# print(aoss.client.search(index=index, size=1))
+# print(vectorstore.client.search(index=index, size=1))
 
 # QA
 question = "What did the president say about Ketanji Brown Jackson"
-# result = aoss.similarity_search(question, k=4)
+# result = vectorstore.similarity_search(question, k=4)
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 qa_chain = RetrievalQA.from_chain_type(
     llm,
-    retriever=aoss.as_retriever()
+    retriever=vectorstore.as_retriever()
 )
 qa_chain({"query": question})
